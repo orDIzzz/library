ac051ea5dc6b74e949f1a38cb1029e183fdfdbe0
orDIzzz <ordizzz2008@yandex.ru>
2019-09-04 14:15:21 +0300

    First commit

---
+++ catalog/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, Bookinstance
+
+# Register your models here.
+# admin.site.register(Author)
+# admin.site.register(Bookinstance)
+# admin.site.register(Book)
+admin.site.register(Genre)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['last_name', 'first_name', ('date_of_birth', 'date_of_death')]
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+
+@admin.register(Bookinstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        })
+    )
